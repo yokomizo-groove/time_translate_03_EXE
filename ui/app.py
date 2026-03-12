f4a165b4d1df06ad64ebee739f32460c9018e65e
yokomizo-groove <60562107+yokomizo-groove@users.noreply.github.com>
2026-03-12 13:02:14 +0900

    Add files via upload

--- ui/app.py
+++ ui/app.py
@@ -7,12 +7,12 @@ import time
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
-
+from ui.load_data_files import load_csv_files
+from ui.check_data_shape import check_header, check_row_structure, check_date_continuity
+from ui.merge_and_sort import merge_and_sort
 from logic.load_file import load_file
-from logic.time_translate_04 import time_translate
-from logic.download_file import to_excel_xlsxwriter
-from logic.download_file import to_excel_fast_numpy
-from logic.download_file import to_csv_fast
+from logic.time_to_number import time_to_number
+from logic.export_file import to_excel_xlsxwriter, to_excel_fast_numpy, to_csv_fast
 
 # ★ 高速 xlsxwriter 版 Excel 変換関数
 #def to_excel_xlsxwriter(df):
@@ -25,67 +25,57 @@ from logic.download_file import to_csv_fast
     
 
 def main():
+ 
     st.title("勤怠データチェックアプリ")
 
-    uploaded_file = st.file_uploader(
-        "CSVまたはExcelファイルをアップロードしてください",
-        type=["csv", "xlsx", "xlsm"]
-    )
+    df1, df2, file1, file2 = load_csv_files()
+    
+    check_header(df1, df2)
+    check_date_continuity(df1, df2)
+    check_row_structure(df1, file1)
+    check_row_structure(df2, file2)
+
+    st.write("CSV1")
+    st.dataframe(df1)
+
+    st.write("CSV2")
+    st.dataframe(df2)
+    
+    st.write("DataFrame化しました")
+
+    base_df = merge_and_sort(df1, df2)
+    st.write("Base_csv_data")
+    st.dataframe(base_df)
+    st.write("Base_dfをDataFrame化しました")
+    # st.stop()
+
 
     # ★ タイマー開始
     start = time.time()
 
-    if uploaded_file is not None:
-        st.success("ファイルを読み込みました")
+    st.write("Translating time to numerics ...")
+    final_array, headers = time_to_number(base_df)
 
-        # ★ タイマー開始
-        start = time.time()
+    csv_bytes = to_csv_fast(final_array, headers)
+
+    st.download_button(
+        label="時刻→数値化後のCSVダウンロード",
+        data=csv_bytes,
+        file_name="temp_output.csv",
+        mime="text/csv"
+    )
+
+      
         
-        df = load_file(uploaded_file)
-        st.write("df.shape:", df.shape)
+    # ★ タイマー終了
+    end = time.time() 
+    elapsed = end - start
 
-        
-        # df2 = time_translate(df)
-
-        # ★ ここで Excel バイト列を作る（高速）
-        # st.write("Making download file with xlsxwriter")
-        # excel_bytes = to_excel_xlsxwriter(df2)
-        st.write("Translating time to numerics ...")
-        final_array, headers = time_translate(df)
-
-        # st.write("Making download file with fast_numpy")
-        # excel_bytes = to_excel_fast_numpy(final_array, headers)
-
-        st.write("Making download file with to_csv_fast")
-        csv_bytes = to_csv_fast(final_array, headers)
-
-        base_name = os.path.splitext(uploaded_file.name)[0]
-        download_name = f"{base_name}_output.xlsx"
-        
-        st.download_button(
-            label="CSVでダウンロード",
-            data=csv_bytes,
-            file_name=f"{base_name}_output.csv",
-            mime="text/csv"
-        )
-
-        
-        # ★ タイマー終了
-        end = time.time() 
-        elapsed = end - start
-
-        # ★ 結果表示
-        st.info(f"処理時間: {elapsed:.2f} 秒")
+    # ★ 結果表示
+    st.info(f"処理時間: {elapsed:.2f} 秒")
         
  
 
-        # ★ ダウンロードボタン
-        # st.download_button(
-        #    label="変換ファイルをダウンロード",
-        #    data=excel_bytes,
-        #    file_name=download_name,
-        #    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-        # )
 
 
 if __name__ == "__main__":
